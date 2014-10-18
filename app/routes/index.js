e55f60d3c76504c4a2a3675bce6b401554a550d2
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-10-18 19:55:34 +0200

    Chapter 4: Routing

---
+++ app/routes/index.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    var questions = [
+      {
+        title:  'How do I feed hamsters?',
+        author: 'Tom Dale'
+      },
+
+      {
+        title:  'Are humans insane?',
+        author: 'Tomster the Hamster'
+      }
+    ];
+
+    return questions;
+  }
+});
