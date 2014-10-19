0f3bac8b5a5f149e70048199fb3e77829e2858c2
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-10-19 22:53:41 +0200

    Chapter 6: CSP

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.contentSecurityPolicy = {
+      'connect-src': "'self' http://localhost:3000",
+    };
   }
 
   if (environment === 'test') {
