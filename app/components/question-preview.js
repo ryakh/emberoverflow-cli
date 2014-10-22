b76feff96b0299f323b983121315b579c6614ba1
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-10-22 21:56:53 +0200

    Chapter 11: question-preview component

---
+++ app/components/question-preview.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'li',
+
+  answersCount: Ember.computed.alias('question.answers.length'),
+
+  pluralForm: function() {
+    var answers = this.get('answersCount');
+    return answers === 1 ? 'answer' : 'answers';
+  }.property('answersCount'),
+
+  hasAnswers: function() {
+    return this.get('answersCount') > 0;
+  }.property('answersCount')
+});
