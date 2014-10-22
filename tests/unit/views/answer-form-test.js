e9eb1f94cdce23c8dcbb92a85f137210731dc47d
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-10-22 22:17:10 +0200

    Chapter 11: answer-form module created

---
+++ tests/unit/views/answer-form-test.js
@@ -0,0 +1,12 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+moduleFor('view:answer-form', 'AnswerFormView');
+
+// Replace this with your real tests.
+test('it exists', function() {
+  var view = this.subject();
+  ok(view);
+});
