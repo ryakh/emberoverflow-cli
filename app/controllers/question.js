62247a13d40de81ae75a4a9b0aff5b0a74139a27
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-10-23 16:52:38 +0200

    Chapter 12: Editing question /controller

--- app/controllers/question.js
+++ app/controllers/question.js
@@ -2,7 +2,22 @@ import Ember from 'ember';
 import SetAuthor from '../mixins/set-author';
 
 export default Ember.ObjectController.extend(SetAuthor, {
+  isEditing: false,
+
+  canEditQuestion: function() {
+    return this.get('author.id') === this.get('currentUser.userToken');
+  }.property('model'),
+
   actions: {
+    toggleEditQuestion: function() {
+      this.toggleProperty('isEditing');
+    },
+
+    submitEdits: function() {
+      this.toggleProperty('isEditing');
+      this.get('model').save();
+    },
+
     answerQuestion: function() {
       var answer = this.store.createRecord('answer', {
         answer: this.get('answer'),
